54280b950c22ccc89a76218361c45670d7081c99
Fabian Losada <fabianlosada2019@gmail.com>
2024-05-17 12:10:00 -0400

    Calculadora de IMC

---
+++ proyecto1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+        float height = 0;
+        float weight = 0;
+        float bmiIndex = 0;
+
+        System.Console.WriteLine ("Ingrese su altura");
+        Single.TryParse (System.Console.ReadLine(), out height);
+
+        System.Console.WriteLine ("Ingrese su peso");
+        Single.TryParse (System.Console.ReadLine(), out weight);
+
+        bmiIndex = weight /(height *height);
+
+        if(bmiIndex < 18.5)
+            Console.WriteLine ("bajo de peso");
+        else if (bmiIndex >=18.5 && bmiIndex < 24.9)
+            Console.WriteLine ("peso ideal");
+        else if (bmiIndex >=25.5 && bmiIndex < 29.9)
+            Console.WriteLine ("sobrepeso");   
+        else if (bmiIndex >=30.0)
+            Console.WriteLine ("Obeso");
