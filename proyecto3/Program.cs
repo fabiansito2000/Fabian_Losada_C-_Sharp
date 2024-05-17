87661acd0808cedf2c3dafd4c3cedfeafb20bf4d
Fabian Losada <fabianlosada2019@gmail.com>
2024-05-17 12:17:03 -0400

    Area y perímetro

---
+++ proyecto3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+            double b, a;
+
+            Console.WriteLine("Ingrese la base: ");
+            b= double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la altura: ");
+            a = double.Parse(Console.ReadLine());
+
+            if (b <= 0 || a <= 0)
+            {
+                Console.WriteLine("Datos incorrectos");
+            }
+            else
+            {
+                double area = b * a;
+
+                double perimetro = 2 * (b + a);
+
+                Console.WriteLine($"Area del rectangulo: {area}");
+
+                Console.WriteLine($"Perimetro del rectangulo: {perimetro}");
+
+            }
+
+
