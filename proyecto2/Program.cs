2e88491d439f8ccb7d98d7e16d7cdaa6c1da08d5
Fabian Losada <fabianlosada2019@gmail.com>
2024-05-17 12:12:40 -0400

    Calculadora Simple

---
+++ proyecto2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+        int num1, num2, resultado;
+        Console.WriteLine("Ingresar el numero 1: ");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingresar el numero 2: ");
+        num2 = int.Parse(Console.ReadLine());
+
+        resultado = num1 + num2;
+        Console.WriteLine("La suma es {0}", resultado);
+        resultado = num1 - num2;
+        Console.WriteLine("La resta es {0}", resultado);
+        resultado = num1 * num2;
+        Console.WriteLine("La multiplicacion es {0}", resultado);
+
+
+            if (num2 !=0)
+            {
+            resultado = num1/num2;
+            Console.WriteLine("La division es {0}", resultado);
+            }
+            else
+            {
+            Console.WriteLine("no se puede dividir un" + "numero entre cero");   
+            }
